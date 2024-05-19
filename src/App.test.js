0981847fff5538fc18338e6916e375130a755523
wcoding <papa5944@gmail.com>
2024-05-19 14:58:39 +0900

    add to

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
 
-test('add two numbers', ()=>{
+test('add two numberss', ()=>{
     expect(2+2).toBe(4)
 })
